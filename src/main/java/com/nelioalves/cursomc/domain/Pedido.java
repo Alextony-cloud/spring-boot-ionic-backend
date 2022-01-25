b5cab527ee3163381bc16d9ec3c97730a85ebedd
Alextony Rodrigues <alextonyrodrigues@gmail.com>
2022-01-25 20:52:13 -0300

    Endpoint /pedidos/{id} disponibilizados

--- src/main/java/com/nelioalves/cursomc/domain/Pedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/Pedido.java
@@ -17,6 +17,9 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Pedido implements Serializable {
 
@@ -27,11 +30,15 @@ public class Pedido implements Serializable {
 	@Basic(optional = false)
 	@Column(name = "id", unique = true, nullable = false)
 	private Integer id;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy hh:ss")
 	private Date instante;
 
+	@JsonManagedReference
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
 
+	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
